413857d2161a2529be1fc9fee3f8978332bbe81e
charafeddinejaafari <charafjaafari49@gmail.com>
2024-12-26 12:35:31 +0100

    Réalisation d'une interface login

--- front-ocr/src/app/app.routes.ts
+++ front-ocr/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
-
+import {LoginComponent} from './login/login.component';
 export const appRoutes: Routes = [
-  { path: '', redirectTo: '/upload', pathMatch: 'full' },
-  { path: 'upload', component: FileUploadComponent },
+  
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'upload', component: FileUploadComponent }
 ];
