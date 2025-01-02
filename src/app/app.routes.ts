948596348d6abd27185f8dbf66bbbf4f47a34049
hossam <hossamchalabi964@gmail.com>
2025-01-02 21:49:46 +0100

    fix route import

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
+import { LoginComponent } from '../../front-ocr/src/app/login/login.component';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { Routes } from '@angular/router';
-import {LoginComponent} from './login/login.component';
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/signup', pathMatch: 'full' },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
